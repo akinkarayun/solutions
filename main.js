9f24a33f6377e8f0dd9884608601b9243d0beb4f
Akin Karayun <akinkarayun1@hotmail.com>
2022-07-25 20:17:02 +0300

    added more challenge that solved

--- main.js
+++ main.js
@@ -42,3 +42,27 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 6, 10, 121, 121]
 // }
 
 // console.log(findDup(arr))
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// const oddArray = numbers.filter((number) => number % 2 !== 0)
+// console.log(oddArray)
+
+// const addNumbers = numbers.reduce((acc, number) => acc + number, 0)
+// console.log(addNumbers)
+
+// let vals = [1, 4, 5, 2, 56, 8, 9]
+// const compare = (a, b) => {
+//   return b - a
+// }
+// console.log(vals.sort((a, b) => compare(a, b)))
+
+const students = [
+  { name: "John James", status: true },
+  { name: "Micheal Obi", status: false },
+  { name: "Bola Ade", status: true },
+  { name: "Emmanuel", status: false },
+  { name: "Faithfulness Alamu", status: true },
+]
+const presentStudent = students.filter((student) => student.status == true)
+console.log(presentStudent)
